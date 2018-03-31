454573f46a7b7ab98b50ae3c014bb91f04311494
Arthur Green <arthur@phraction.org>
2018-03-31 17:44:28 +0100

    Set logging level to ERROR

--- traveller_api/t5/cargogen/trade_cargo.py
+++ traveller_api/t5/cargogen/trade_cargo.py
@@ -6,7 +6,7 @@ import logging
 from T5_worldgen.planet import Planet
 
 LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.DEBUG)
+LOGGER.setLevel(logging.ERROR)
 
 
 class FluxRoll(object):
