35c2af5eddbeb6e6521d240832e57eab103f4290
Arthur Green <arthur@phraction.org>
2018-04-04 16:30:24 +0100

    Fix  build script

--- scripts/build-docker-image.sh
+++ scripts/build-docker-image.sh
@@ -6,10 +6,10 @@ docker_tag=`git rev-parse --abbrev-ref HEAD`
 image_name='egor045/traveller_rest_api'
 docker_file='Dockerfile'
 
-echo 'Building image $image_name:$branch from $docker_file'
+echo 'Building image '$image_name:$docker_tag' from '$docker_file
 
 /usr/bin/docker build \
-    -t $image_name:$branch \
+    -t $image_name:$docker_tag \
     -f $docker_file \
     .
 
