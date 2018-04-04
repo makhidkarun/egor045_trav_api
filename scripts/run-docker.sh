b5a0590500f3d044450f4b2ccd730ac46f2c1454
Arthur Green <arthur@phraction.org>
2018-04-04 17:38:21 +0100

    Run Docke script

---
+++ scripts/run-docker.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+# Args:
+#   -p port(s)
+#   -e environment var(s)
+
+ENV="prometheus_multiproc_dir=/metrics"
+PORTS="8000:8000"
+IMAGE="egor045/traveller_rest_api:latest"
+
+POSITIONAL=()
+while [[ $# -gt 0 ]] ; do
+    key="$1"
+    case $key in
+        -e|--env)
+            ENV="$2"
+            shift
+            shift;;
+        -p|--publish)
+            PORTS="$2"
+            shift
+            shift
+            ;;
+        *)    # unknown option
+            POSITIONAL+=("$1") # save it in an array for later
+            shift # past argument
+            ;;
+    esac
+done
+
+if [ "$POSITIONAL" != "" ] ; then
+    IMAGE="$POSITIONAL"
+fi
+
+set -- "${POSITIONAL[@]}"
+echo Running docker image $IMAGE with env=$ENV, port=$PORTS
+
+echo /usr/bin/docker run --rm -p $PORTS -e $ENV $IMAGE
