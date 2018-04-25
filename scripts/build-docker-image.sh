cd3b5be2003837cb75ff2b99ca7aab0a22fb41e4
Arthur Green <arthur@phraction.org>
2018-04-25 16:10:17 +0100

    Checked out from master

--- scripts/build-docker-image.sh
+++ scripts/build-docker-image.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 
 # Get current branch (use in docker build)
-
-docker_tag=`git rev-parse --abbrev-ref HEAD`
-image_name='egor045/traveller_rest_api'
+git_latest_commit=`git rev-parse HEAD`
+git_branch=`git rev-parse --abbrev-ref HEAD`
+docker_tag="$git_branch_$git_latest_commit"
+# image_name='egor045/traveller_rest_api'
+image_name='traveller_rest_api'
 docker_file='Dockerfile'
 
 echo 'Building image '$image_name:$docker_tag' from '$docker_file
