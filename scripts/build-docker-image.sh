d20e9f211984816a24ee3df25cbf6428095aaaf8
Arthur Green <arthur@phraction.org>
2018-05-06 17:07:49 +0100

    More sync with master

--- scripts/build-docker-image.sh
+++ scripts/build-docker-image.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 # Get current branch (use in docker build)
+WRKDIR=/home/arthur/projects/egor-45_trav_api
 git_latest_commit=`git rev-parse HEAD`
 git_branch=`git rev-parse --abbrev-ref HEAD`
 docker_tag="$git_branch_$git_latest_commit"
@@ -8,6 +9,11 @@ docker_tag="$git_branch_$git_latest_commit"
 image_name='traveller_api'
 docker_file='Dockerfile'
 
+cd $WRKDIR
+
+cat ./scripts/api_version.py_template | \
+    sed -e "s/GIT_LATEST_COMMIT/$git_latest_commit/" > \
+    traveller_api/api_version.py
 echo 'Building image '$image_name:$docker_tag' from '$docker_file
 
 /usr/bin/docker build \
